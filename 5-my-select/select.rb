cc2b9de5488e1f254eefaffa1c838cf14c3ad6b2
Vincent Stridh <vincent.stridh@elev.ga.ntig.se>
2026-09-07 11:03:47 +0200

    initial commit

--- 5-my-select/select.rb
+++ 5-my-select/select.rb
@@ -2,11 +2,6 @@ class Array
   def my_select(&block)
     i = 0
     out = []
-    #while i < self.length
-    #  x = block.call(self[i])
-    #  p x 
-    #  i = i.succ
-    #end
     while i < self.length
       x = block.call(self[i])
       if x == true
@@ -16,8 +11,21 @@ class Array
     end
     out
   end
+
+  def my_map(&block)
+    i = 0
+    out = []
+    while i < self.length
+      x = block.call(self[i])
+      out << x
+      i = i.succ
+    end
+    out
+  end
 end
 
 p [1,2,3,4].my_select { |x| x.odd? }
 
 p [1,2,3,4].my_select { |x| x.odd? } == [1,2,3,4].select { |x| x.odd? }
+
+p [1,2,3].my_map { |x| x * x }
